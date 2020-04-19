8bd140875f4856d18d8604ef5e87a73725956f72
singo <jinwusongmu20@gmail.com>
2020-04-19 20:59:50 +0900

    ma

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,17 +8,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = @group.messages.new(message_params)
-    if @message.save
-      respond_to do |format|
-        format.json
-      end
-
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
-    end
+    
   end
 
   private
