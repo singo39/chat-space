56b4614a1d1fe664f1eb902d91dea75347e5e832
singo <jinwusongmu20@gmail.com>
2020-04-21 16:53:36 +0900

    自動更新機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :new, :create, :edit, :update] do
+  
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
- end
- 
+end
+
 
